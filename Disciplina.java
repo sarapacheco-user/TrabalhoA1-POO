50519422b47b22528ac1e769c0bd3f4e24348cdb
Sara Pacheco de Azevedo <thesarapacheco@gmail.com>
2024-10-03 13:54:02 -0300

    Create Disciplina.java

---
+++ Disciplina.java
@@ -0,0 +1,13 @@
+class Disciplina {
+    // atributos
+    
+    private String nome;
+
+    public  Disciplina(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    } 
+}
