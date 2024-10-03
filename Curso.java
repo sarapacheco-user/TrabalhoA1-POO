5260ffb7c2e3b899afc262ff92e47a34561b7903
Sara Pacheco de Azevedo <thesarapacheco@gmail.com>
2024-10-03 13:52:51 -0300

    Create Curso.java

---
+++ Curso.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Curso {
+    private String nome;
+    private List<Disciplina> disciplinas;
+
+    public Curso(String nome) {
+        this.nome = nome;
+        this.disciplinas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void adicionarDisciplina(Disciplina disciplina) {
+        disciplinas.add(disciplina);
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+}
+
