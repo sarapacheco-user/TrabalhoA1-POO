fd9cbe02012221fbbcafbb5645f8eb89bf9e0451
Sara Pacheco de Azevedo <thesarapacheco@gmail.com>
2024-10-03 13:56:50 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,12 @@
+class Pessoa {
+    // atributo
+    private String nome;
+    // construtor
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+    // obtenho o nome da pessoa
+    public String getNome() {
+        return nome;
+    }
+}
